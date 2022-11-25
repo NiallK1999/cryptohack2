30580cf3f99a21ca2c3ced292b1cbe5f15aa6507
NiallK1999 <B00121114@mytudublin.ie>
2022-11-25 17:53:36 -0500

    25th nov lab

---
+++ Symmetric Ciphers/matrix_e1b463dddbee6d17959618cf370ff1a5.py
@@ -0,0 +1,23 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    text = '' 
+    for row in range(4):
+        for col in range(4):
+            text += chr(matrix[row][col])
+
+    return text
+
+matrix2bytes
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(add))
